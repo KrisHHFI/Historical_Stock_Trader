16f75edeb68a30c951c0653a20875dd1795ebe69
KrisHHFI <kristopherpepperuk@gmail.com>
2026-03-07 00:23:36 +0200

    Clearer way of setting algo

--- constants.py
+++ constants.py
@@ -1,5 +1,9 @@
+from utils.trading_algorithms.run_mock_ema_crossover_backtest import run_mock_ema_crossover_backtest
+#from utils.trading_algorithms.run_mock_vwap_ema_crossover_backtest import run_mock_vwap_ema_crossover_backtest
+
 raw_data_folder: str = "/Users/kristopherpepper/Documents/jupyterProjects/historicalStockTrader2/raw_data"
 
 # Trading
 capital: int = 10000
 transaction_fee_bps: float = 1.0
+active_algorithm = run_mock_ema_crossover_backtest
