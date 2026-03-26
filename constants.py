0c4447125733500b4bcfb017af50fd48ddf6028f
KrisHHFI <kristopherpepperuk@gmail.com>
2026-03-26 23:06:34 +0200

    minor changes

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
 from pathlib import Path
-from utils.trading_algorithms.run_mock_statistical_arbitrage_backtest import run_mock_statistical_arbitrage_backtest
+from utils.trading_algorithms.run_mock_market_making_backtest import run_mock_market_making_backtest
 
 raw_data_folder: str = "/Users/kristopherpepper/Documents/jupyterProjects/historicalStockTrader2/raw_data"
 
 # Trading
 capital: int = 10000
 transaction_fee_bps: float = 1.0
-active_algorithm = run_mock_statistical_arbitrage_backtest
+active_algorithm = run_mock_market_making_backtest
 
 # ML Optimizer
-ml_n_trials: int = 40
+ml_n_trials: int = 30
 ml_trial_delay_seconds: float = 0.5   # pause between trials to reduce CPU heat
 ml_results_file: Path = Path(__file__).parent / "ml_optimizer_results.txt"
 ml_best_params_file: Path = Path(__file__).parent / "ml_best_params.json"
